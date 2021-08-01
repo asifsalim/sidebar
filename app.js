b56357b2734992a7df7c7477931db074aef4ccc8
asifsalim <sojolua36@gmail.com>
2021-08-02 04:11:35 +0600

    full responsive

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-//hello
+const sideBar = document.querySelector(".sidebar");
+const crossBtn = document.querySelector(".cross-icon");
+const menuBtn = document.querySelector(".menu-icon");
 
+crossBtn.addEventListener('click',function(){
+    sideBar.classList.toggle('hidebar');
+})
+
+menuBtn.addEventListener('click',function(){
+    sideBar.classList.toggle('hidebar');
+})
