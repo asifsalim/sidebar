331e688d8e932523b8899753e0b670db7f5a79a2
asifsalim <sojolua36@gmail.com>
2021-08-01 20:20:30 +0600

    changes

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const navToggle = document.querySelector('.show-links');
-const links = document.querySelector('.link-box')
-
-navToggle.addEventListener('click',function(){
-    links.classList.toggle('show-box');
-})
+//hello
 
